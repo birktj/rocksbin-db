4c9aec9684c01d6221da160c728e85d894b309b1
Birk Tjelmeland <birktjelmeland@yahoo.no>
2018-11-15 23:09:33 +0100

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,225 @@
+extern crate rocksdb;
+extern crate bincode;
+extern crate serde;
+
+use serde::{Serialize, de::DeserializeOwned};
+
+use std::sync::Arc;
+use std::marker::PhantomData;
+use std::path::Path;
+
+#[derive(Debug)]
+pub enum ErrorKind {
+    Bincode(bincode::Error),
+    Rocksdb(rocksdb::Error),
+}
+
+pub type Error = Box<ErrorKind>;
+pub type Result<T> = ::std::result::Result<T, Error>;
+
+impl From<bincode::Error> for Error {
+    fn from(e: bincode::Error) -> Error {
+        Box::new(ErrorKind::Bincode(e))
+    }
+}
+
+impl From<rocksdb::Error> for Error {
+    fn from(e: rocksdb::Error) -> Error {
+        Box::new(ErrorKind::Rocksdb(e))
+    }
+}
+
+#[derive(Clone)]
+pub struct DB {
+    db: Arc<rocksdb::DB>
+}
+
+impl DB {
+    pub fn open<P: AsRef<Path>>(path: P) -> Result<DB> {
+        Ok(DB {
+            db: Arc::new(rocksdb::DB::open_default(path)?)
+        })
+    }
+
+    pub fn prefix<K: Serialize + DeserializeOwned, V: Serialize + DeserializeOwned>(&self, prefix: &[u8]) -> Prefix<K, V> {
+        Prefix {
+            db: self.db.clone(),
+            prefix: prefix.to_owned(),
+            _k: PhantomData,
+            _v: PhantomData,
+        }
+    }
+}
+
+#[derive(Clone)]
+pub struct Prefix<K, V> {
+    db: Arc<rocksdb::DB>,
+    prefix: Vec<u8>,
+    _k: PhantomData<K>,
+    _v: PhantomData<V>,
+}
+
+impl<K: Serialize + DeserializeOwned, V: Serialize + DeserializeOwned> Prefix<K, V> {
+    pub fn get(&self, key: &K) -> Result<Option<V>> {
+        let mut key_buf = self.prefix.clone();
+        key_buf.reserve(bincode::serialized_size(&key)? as usize);
+        bincode::serialize_into(&mut key_buf, &key)?;
+        match self.db.get(&key_buf)? {
+            Some(data) => Ok(Some(bincode::deserialize(&data)?)),
+            None => Ok(None),
+        }
+    }
+
+    pub fn insert(&self, key: &K, value: &V) -> Result<()> {
+        let mut key_buf = self.prefix.clone();
+        key_buf.reserve(bincode::serialized_size(&key)? as usize);
+        bincode::serialize_into(&mut key_buf, &key)?;
+        let value_buf = bincode::serialize(value)?;
+
+        self.db.put(&key_buf, &value_buf)?;
+        Ok(())
+    }
+
+    pub fn remove(&self, key: &K) -> Result<()> {
+        let mut key_buf = self.prefix.clone();
+        key_buf.reserve(bincode::serialized_size(&key)? as usize);
+        bincode::serialize_into(&mut key_buf, &key)?;
+
+        self.db.delete(&key_buf)?;
+        Ok(())
+    }
+
+    pub fn contains_key(&self, key: &K) -> Result<bool> {
+        self.get(key).map(|v| v.is_some()) // TODO: optimize
+    }
+
+    pub fn modify<F: FnOnce(&mut V)>(&self, key: &K, f: F) -> Result<()> {
+        match self.get(key)? {
+            Some(mut value) => {
+                f(&mut value);
+                self.insert(&key, &value)
+            }
+            None => Ok(())
+        }
+    }
+
+    pub fn iter(&self) -> Iter<K, V> {
+        let mut db_iter = self.db.raw_iterator();
+        db_iter.seek(&self.prefix);
+
+        Iter {
+            db_iter,
+            prefix: self.prefix.clone(),
+            _k: PhantomData,
+            _v: PhantomData,
+        }
+    }
+
+    pub fn keys(&self) -> Keys<K> {
+        let mut db_iter = self.db.raw_iterator();
+        db_iter.seek(&self.prefix);
+
+        Keys {
+            db_iter,
+            prefix: self.prefix.clone(),
+            _k: PhantomData,
+        }
+    }
+
+    pub fn values(&self) -> Values<V> {
+        let mut db_iter = self.db.raw_iterator();
+        db_iter.seek(&self.prefix);
+
+        Values {
+            db_iter,
+            prefix: self.prefix.clone(),
+            _v: PhantomData,
+        }
+    }
+}
+
+pub struct Iter<K, V> {
+    db_iter: rocksdb::DBRawIterator,
+    prefix: Vec<u8>,
+    _k: PhantomData<K>,
+    _v: PhantomData<V>,
+}
+
+impl<K: DeserializeOwned, V: DeserializeOwned> Iterator for Iter<K, V> {
+    type Item = Result<(K, V)>; // :(
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.db_iter.valid() {
+            let k =
+                // We do not reuse the buffer so this is safe
+                unsafe {self.db_iter.key_inner()}
+                    .and_then(|k| if &k[0..self.prefix.len()] == &self.prefix[..] { Some(k) } else { None } )
+                    .map(|k| bincode::deserialize(&k[self.prefix.len()..]));
+            let v =
+                // We do not reuse the buffer so this is safe
+                unsafe {self.db_iter.value_inner()}
+                    .map(|k| bincode::deserialize(k));
+
+            self.db_iter.next();
+            k.and_then(|k| v.map(|v| Ok((k?, v?))))
+        }
+        else {
+            None
+        }
+    }
+}
+
+pub struct Keys<K> {
+    db_iter: rocksdb::DBRawIterator,
+    prefix: Vec<u8>,
+    _k: PhantomData<K>,
+}
+
+impl<K: DeserializeOwned> Iterator for Keys<K> {
+    type Item = Result<K>; // :(
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.db_iter.valid() {
+            let k =
+                // We do not reuse the buffer so this is safe
+                unsafe {self.db_iter.key_inner()}
+                    .and_then(|k| if &k[0..self.prefix.len()] == &self.prefix[..] { Some(k) } else { None } )
+                    .map(|k| Ok(bincode::deserialize(&k[self.prefix.len()..])?));
+
+            self.db_iter.next();
+            k
+        }
+        else {
+            None
+        }
+    }
+}
+
+pub struct Values<V> {
+    db_iter: rocksdb::DBRawIterator,
+    prefix: Vec<u8>,
+    _v: PhantomData<V>,
+}
+
+impl<V: DeserializeOwned> Iterator for Values<V> {
+    type Item = Result<V>; // :(
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.db_iter.valid() {
+            let v =
+                // We do not reuse the buffer so this is safe
+                unsafe {self.db_iter.key_inner()}
+                    .and_then(|k| if &k[0..self.prefix.len()] == &self.prefix[..] { Some(k) } else { None } )
+                    .and_then(|_| 
+                        unsafe {self.db_iter.value_inner()}
+                            .map(|v| Ok(bincode::deserialize(v)?))
+                        );
+
+            self.db_iter.next();
+            v
+        }
+        else {
+            None
+        }
+    }
+}
